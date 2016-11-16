c9c3ee7623aa23877273f2bb78c91d23306434ab
Oli <olimoorow@gmail.com>
2016-11-16 15:48:33 +0000

    posts controller

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,33 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth');
+const postsController = require('../controllers/posts');
+const usersController = require('../controllers/users');
 
 router
   .post('/login', authController.login)
   .post('/register', authController.register);
+
+router.route('/posts/:id')
+  .get(postsController.show)
+  .post(postsController.create)
+  .put(postsController.update)
+  .delete(postsController.delete);
+
+router.route('/posts/user/feed/:id')
+  .get(postsController.indexPublic);
+
+router.route('/posts/user/:id')
+  .get(postsController.indexPrivate);
+
+router.route('/users/:id/following')
+  .get(usersController.following);
+
+router.route('/users/:id/followers')
+  .get(usersController.followers);
+
+router.route('/users/:id')
+  .get(usersController.show)
+  .put(usersController.update)
+  .delete(usersController.delete);
 
 module.exports = router;
